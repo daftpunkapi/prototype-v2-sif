6724923fa0174370a3df99de578ebba7de1908c9
Raghav Mehta <Raghav@MacBook-Pro.local>
2023-07-01 13:12:37 +0530

    sources connected

--- flink.py
+++ flink.py
@@ -15,9 +15,7 @@ def log_processing():
         CREATE TABLE mouse_clicks(
             sessionId VARCHAR,
             event VARCHAR,
-            timestamp_clicks BIGINT,
-            time_ltz AS TO_TIMESTAMP_LTZ(timestamp_clicks, 3),
-            WATERMARK FOR timez_ltz AS timez_ltz - INTERVAL '5' SECONDS
+            timestamp_clicks BIGINT
         ) WITH (
             'connector' = 'kafka',
             'topic' = 'mouse_clicks',
@@ -32,28 +30,30 @@ def log_processing():
         """    
     t_env.execute_sql(source_ddl)
     tbl = t_env.from_path('mouse_clicks')
+    tbl.print_schema()
+    tbl.execute().print()
 
-    windowed_clicks = (
-    tbl.window(Tumble.over(lit(5).seconds).on(col("timez_ltz")).alias("w"))
-    .group_by(col("sessionId"))
-    .select(
-        col("sessionId").start.alias("window_start"),
-        col("sessionId").end.alias("window_end"),
-        col("event").count.alias("count")
-        )
-    )
+    # windowed_clicks = (
+    # tbl.window(Tumble.over(lit(5).seconds).on(col("timez_ltz")).alias("w"))
+    # .group_by(col("sessionId"))
+    # .select(
+    #     col("sessionId").start.alias("window_start"),
+    #     col("sessionId").end.alias("window_end"),
+    #     col("event").count.alias("count")
+    #     )
+    # )
 
-    sink_ddl_print = """
-        CREATE TABLE printx (
-        `window_start` TIMESTAMP(3),
-        `window_end` TIMESTAMP(3),
-        `count` BIGINT
-    ) WITH (
-        'connector' = 'print'
-    )
-    """
-    t_env.execute_sql(sink_ddl_print)
-    windowed_clicks.execute_insert('printx').wait()
+    # sink_ddl_print = """
+    #     CREATE TABLE printx (
+    #     `window_start` TIMESTAMP(3),
+    #     `window_end` TIMESTAMP(3),
+    #     `count` BIGINT
+    # ) WITH (
+    #     'connector' = 'print'
+    # )
+    # """
+    # t_env.execute_sql(sink_ddl_print)
+    # windowed_clicks.execute_insert('printx').wait()
 
 
 if __name__ == '__main__':
